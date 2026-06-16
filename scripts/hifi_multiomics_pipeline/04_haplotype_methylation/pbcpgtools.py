8914f2e22494e6c1d246354716d598331a38912e
Arlen James Mosquera Ruiz <150406790+ArlenJames07@users.noreply.github.com>
2026-06-16 11:10:23 -0500

    Update pbcpgtools.py

--- scripts/hifi_multiomics_pipeline/04_haplotype_methylation/pbcpgtools.py
+++ scripts/hifi_multiomics_pipeline/04_haplotype_methylation/pbcpgtools.py
@@ -1,9 +1,16 @@
-from pathlib import Path
-import runpy
+import os, subprocess
 
-
-SOURCE = Path(__file__).resolve().parents[2] / "methylation_genomes" / "pbcpgtools.py"
-
-
-if __name__ == "__main__":
-    runpy.run_path(str(SOURCE), run_name="__main__")
+def pbcpgtools():
+    ruta_bam_alineados="/mnt/diskrare/arlenb/08/hiphase_results/bamfiles"
+    programa="/home/rare/programs/pb-CpG-tools-v2.3.2-x86_64-unknown-linux-gnu/bin/aligned_bam_to_cpg_scores"
+    modelo="/home/rare/programs/pb-CpG-tools-v2.3.2-x86_64-unknown-linux-gnu/models/pileup_calling_model.v1.tflite"
+    ruta_salida="/home/rare/arlen/outputs/methylation/genomes_2"
+    os.makedirs(ruta_salida, exist_ok=True)
+    for x in os.listdir(ruta_bam_alineados):
+        if x.endswith("08_1_A01_bc2043_001P.bam"):
+            file=os.path.join(ruta_bam_alineados,x)
+            basename=os.path.basename(file).replace(".bam","")
+            cmd=f"{programa} --bam {file} --output-prefix {ruta_salida}/{basename} --model {modelo} --threads 32"
+            subprocess.run(cmd, check=True, shell=True)
+            
+pbcpgtools()            
